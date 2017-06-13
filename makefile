073864655a720ed3235d7bc54b5d3be2ff8e81b6
qngapparat <jakob.wallnoefer@outlook.com>
2017-06-13 21:38:44 +0200

    init

--- makefile
+++ makefile
@@ -1,2 +1,3 @@
 all:
 	gcc membench.c -o membench.out -pthread
+	gcc malloc.c -o malloc.out
---
+++ malloc.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//linked list to track freed, previously allocated blocks.
+struct list_entry {
+    size_t size;
+    struct list_entry* next;
+};
+
+typedef struct list_entry list_entry_t;
+
+
+static list_entry_t free_block_list_head = { 0, NULL };
+static const size_t overhead = sizeof(size_t);
+static const size_t align_to = 16;
+
+void* my_malloc(size_t size) {
+    //ceiling-round size+sizeof(size_t) to the nearest multiple of align_to
+    size = (size + sizeof(size_t) + (align_to - 1)) & ~ (align_to - 1);
+    list_entry_t* block = free_block_list_head.next;
+    list_entry_t** head = &(free_block_list_head.next);
+    while (block != NULL) {
+        if (block->size >= size) {
+            *head = block->next;
+            return ((char*)block) + sizeof(size_t);
+        }
+        head = &(block->next);
+        block = block->next;
+    }
+
+    block = (list_entry_t*)sbrk(size);
+    block->size = size;
+
+    return ((char*)block) + sizeof(size_t);
+}
+
+void free(void* ptr) {
+    list_entry_t* block = (list_entry_t*)(((char*)ptr) - sizeof(size_t));
+    block->next = free_block_list_head.next;
+    free_block_list_head.next = block;
+}
+
+
+
+int main(int argc, char const *argv[]) {
+
+    int* integer = my_malloc(sizeof(int));
+    *integer = 10;
+    printf("integer: %d\n", *integer);
+    free(integer);
+    printf("freed. done\n");
+
+    return EXIT_SUCCESS;
+}
